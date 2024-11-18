da3d22fe057a56a8156b1041265d5b1709bb949e
toufikul-islam <toufikul.cse@gmail.com>
2024-11-18 14:40:21 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'docker-project';
+   tasks: { title: string; completed: boolean }[] = [];
+  newTask: string = '';
+
+  // Add a new task
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ title: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  // Toggle completion status of a task
+  toggleTaskCompletion(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  // Delete a task
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
